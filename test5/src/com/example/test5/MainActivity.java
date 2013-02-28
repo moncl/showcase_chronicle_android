40831b2a8259076da5b3a892aa99c2399f596302
장인수(Jang Insoo) <oasisholics@gmail.com>
2013-02-28 15:47:16 +0900

    url이미지 불러오기

--- test5/src/com/example/test5/MainActivity.java
+++ test5/src/com/example/test5/MainActivity.java
@@ -4,7 +4,10 @@ import com.loopj.android.image.SmartImageView;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.DisplayMetrics;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -14,8 +17,17 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	
 		SmartImageView myImage = (SmartImageView) this.findViewById(R.id.my_image);
-		myImage.setImageUrl("http://images.nationalgeographic.com/wpf/media-live/photos/000/004/cache/african-elephant_435_600x450.jpg");
+		myImage.setImageUrl("http://uiimage.dnshop.co.kr/eventfile/C803_20100706105949/20100809_SOUP.jpg");
 		//myImage.setImageContact(contactAddressBookId);
+	
+		DisplayMetrics displayMetrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+		int deviceWidth = displayMetrics.widthPixels;
+		int deviceHeight =displayMetrics.heightPixels;
+		
+		TextView size_text=(TextView) findViewById(R.id.text);
+		size_text.setText(deviceHeight);
+		size_text.setText(deviceWidth);
 	}
 
 	@Override
